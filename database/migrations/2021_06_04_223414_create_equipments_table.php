48dde1080123bd1a9e9a440a0a46a99312b5b965
yuki1923 <kygarcons0629@gmail.com>
2021-06-05 14:12:46 +0900

    add:マイグレーションファイルの作成

---
+++ database/migrations/2021_06_04_223414_create_equipments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('equipment_name');
+            $table->string('storage_location');
+            $table->integer('quantity');
+            $table->date('notification_date');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+}
